fd9ba4d56ac160440e422d9b9859e9bd9ba4183d
stevchow <stephen_chow97@yahoo.co.id>
2018-03-04 16:22:03 +0700

    done easy hard +3circles

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-let colors = generateRandomColor(6);
+let numSquares = 9;
+let colors = generateRandomColor(numSquares);
 const squares = document.querySelectorAll(".square");
 const displayRGB = document.querySelector("#displayRGB");
 const msgDisplay = document.querySelector("#message");
 const resetButton = document.querySelector("#reset");
+const easyBtn = document.querySelector("#easyBtn");
+const hardBtn = document.querySelector("#hardBtn");
 let pickedColor = pickColor();
 const h1 = document.querySelector("h1");
 
 displayRGB.textContent = pickedColor.toUpperCase();
 
+easyBtn.addEventListener("click", function() {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColor(numSquares);
+  pickedColor = pickColor();
+  displayRGB.textContent = pickedColor.toUpperCase();
+  //push random color to each square
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) squares[i].style.backgroundColor = colors[i];
+    else squares[i].style.display = "none";
+  }
+  h1.style.backgroundColor = "rgb(233, 224, 224)"
+});
+hardBtn.addEventListener("click", function() {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numSquares = 9;
+  colors = generateRandomColor(numSquares);
+  pickedColor = pickColor();
+  displayRGB.textContent = pickedColor.toUpperCase();
+  //push random color to each square
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+  h1.style.backgroundColor = "rgb(233, 224, 224)"
+});
+
 resetButton.addEventListener("click", function reset() {
   //generate random color
-  colors = generateRandomColor(6);
+  colors = generateRandomColor(numSquares);
   //assign new pickedColor
   pickedColor = pickColor();
   displayRGB.textContent = pickedColor.toUpperCase();
